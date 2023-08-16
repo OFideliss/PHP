71835aefa9acadb8d25426435fd33666591a0192
OFideliss <otavio-fidelis@outlook.com>
2023-08-16 18:57:31 -0300

    16/08/23

---
+++ teste/teste.php
@@ -0,0 +1,29 @@
+<?php
+
+echo "2 + 2 = ";
+echo 2+2;
+echo "<br>";
+echo "2 x 2 = ";
+echo 2*2;
+echo "<br>";
+echo "2 / 2 = ";
+echo 2/2;
+echo "<br>";
+echo "<br>";
+echo "Oi == Hello => ";
+var_dump("Oi"=="Hello");
+echo "<br>";
+echo "<br>";
+echo "2 é diferente de 3 => ";
+var_dump(2!=3);
+echo "<br>";
+echo "Eu estou";
+echo "<br>";
+echo "Programando....";
+echo "<br>";
+echo "Em php";
+echo "<br>";
+echo "Dominando....";
+echo "<br>";
+echo "Dev web!!";
+?>
