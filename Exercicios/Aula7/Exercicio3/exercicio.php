70bcdd112b6cdfb560b6782097aae97e85a1db70
OFideliss <otavio-fidelis@outlook.com>
2023-08-28 21:57:05 -0300

    28/08/23

--- Exercicios/Aula7/Exercicio3/exercicio.php
+++ Exercicios/Aula7/Exercicio3/exercicio.php
@@ -3,17 +3,17 @@
 function exercicio ($salarioHora,$horasMes) {
 
 $salarioBruto = $salarioHora*$horasMes;
-$ir = $salarioBruto*0.11;
-$inss = $salarioBruto*0.08;
-$sindicato = $salarioBruto*0.05;
-$salarioLiquido = $salarioBruto - $ir - $inss - $sindicato;
+$ir = $salarioBruto*0.11;   // Valor do desconto de Imposto de Renda (IR)
+$inss = $salarioBruto*0.08; // Valor do desconto de INSS
+$sindicato = $salarioBruto*0.05; // Valor do desconto de sindicato
+$salarioLiquido = $salarioBruto - $ir - $inss - $sindicato; // Sálario Liquido
 echo "<br>";
 
-return "Salario bruto: R$$salarioBruto,00 <br>
-Descontos :( : <br>
-INSS: R$$inss,00 <br>
-Sindicato: R$$sindicato,00 <br>
-Salario liquido: R$$salarioLiquido,00
+return "+ Salario bruto:......R$$salarioBruto,00 <br>
+- IR:........................R$$ir,00 <br>
+- INSS:...................R$$inss,00 <br>
+- Sindicato..............R$$sindicato,00 <br>
+- Salario liquido:....R$$salarioLiquido,00
 ";
 }
 
--- Exercicios/Aula7/Exercicio3/formulario.php
+++ Exercicios/Aula7/Exercicio3/formulario.php
@@ -1,6 +1,6 @@
 <?php
-include  "exercicio.php";
+include  "exercicio.php"; // incluir o arquivo em que esta a function
 $salarioHora = $_POST['salarioHora'];  // pega o valor digitado na pagina html
 $horasMes = $_POST['horasMes'];     // pega o valor digitado na pagina html
-echo exercicio($salarioHora,$horasMes); // chama a função do arquivo exercicio1.php
+echo exercicio($salarioHora,$horasMes); // chama a função do arquivo exercicio.php
 ?>
