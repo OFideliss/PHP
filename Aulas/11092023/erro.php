6511f10093fa2ce94241c4ac699d76b5dcf4d8cc
OFideliss <otavio-fidelis@outlook.com>
2023-09-12 21:51:18 -0300

    12/09/23

---
+++ Aulas/11092023/erro.php
@@ -0,0 +1,11 @@
+<?php
+// Cabeçalho da página
+header('Content-Type: text/html; charset=utf-8');
+
+// Imprime uma mensagem de sucesso
+echo '<h1> Ocorreu um erro ao realizar o cadastro!</h1>';
+
+// Imprime um link para a página principal
+echo '<a href="index.html">Voltar para a página principal</a>';
+
+?>
--- Aulas/11092023/form.php
+++ Aulas/11092023/form.php
@@ -4,17 +4,42 @@
 $servename = "localhost";
 $username = "root";
 $password = "";
-$dbname = "cadastro";
+$dbname = "clientes";
 
 // Criar conexão com DB
-$conn = mysqli_connect($servename, $username, $password, $dbname);
+$conexao = mysqli_connect($servename, $username, $password, $dbname);
 
 //Verificando a conexão
-if (!$conn){
+if (!$conexao){
 	die("Falha na conexão!.". mysqli_connect_error());
 }
 
 echo "Conectado com sucesso!";
+//=============================Formulario de Cadastro=================
 
-mysqli_close($conn);
+//Verifica se a conexão foi bem sucedida
+if ($conexao) {
+	// Recupera os dados do formulário
+	$nome =$_POST['nome'];
+	$email =$_POST['email'];
+	$data_nascimento=$_POST['data_nascimento'];
+    $endereco=$_POST['endereco'];
+
+	$data = implode("-", array_reverse(explode("/", $data_nascimento)));
+
+    // Insere o dados na tabela do banco
+    $sql = "INSERT INTO usuario(nome, email, dataNasc, endereco) VALUES ('$nome', '$email','$data', '$endereco')";
+    mysqli_query($conexao,$sql);
+
+    // Verifica se a inserção foi bem-sucedida
+    if(mysqli_affected_rows($conexao) > 0 ){
+
+    	//Redireciona para uma página de sucesso
+    	header('Location: sucesso.php');
+    } else {
+    	//Redireciona para uma página de erro
+    	header('Location: erro.php');
+    }  
+}
+mysqli_close($conexao);
 ?>
